8856dc65be0ff884e4d41d23d94ff2f5ecb4ea41
Mollie Christensen <mschristens4@wisc.edu>
2021-09-17 20:00:11 -0500

    Initial commit

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -11,5 +11,3 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-
-    Run git config --global user.email "you@example.com" git config --global user.name "Your Name" to set your account's default identity. Omit --global to set the identity only in this repository. unable to auto-detect email address (got 'chris@DESKTOP-U2HENKK.(none)')
